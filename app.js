e648954917f866cc2b6b404ea4dc2e61dc3c9d5e
king610160 <king610160@gmail.com>
2022-10-16 18:33:52 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -4,20 +4,11 @@ const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
 const methodOverride = require("method-override")
-const mongoose = require("mongoose")
 const Restaurant = require("./models/restaurant")
 const routes = require('./routes')
 
-mongoose.connect(process.env.MONGODB_URI, {  useNewUrlParser: true,  useUnifiedTopology: true,})
-const db = mongoose.connection
+require('./config/mongoose')
 
-db.on("error", () => {
-  console.log("mongodb error!")
-})
-
-db.once("open", () => {
-  console.log("mongodb connected!")
-})
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
--- models/seed/restaurantSeeder.js
+++ models/seed/restaurantSeeder.js
@@ -1,17 +1,6 @@
-const mongoose = require("mongoose")
+const db = require('../../config/mongoose')
 const restaurant = require("../restaurant")
 const restaurantList = require("../../restaurant.json").results
-
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-
-const db = mongoose.connection
-
-db.on("error", () => {
-  console.log("mongodb error!")
-})
 
 db.once("open", () => {
   console.log("running restaurantSeeder script...")
