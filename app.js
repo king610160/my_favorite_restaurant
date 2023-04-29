16cd8c260a269e936fbba73017101d1d7edbba0a
king610160 <king610160@gmail.com>
2023-04-29 18:19:01 +0800

    feat:change handlebars to hbs, add env file, add users router

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 // require packages used in the project
 const express = require('express')
 const app = express()
@@ -7,11 +8,17 @@ const methodOverride = require("method-override")
 const Restaurant = require("./models/restaurant")
 const routes = require('./routes')
 
+
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 require('./config/mongoose')
 
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
+app.set('view engine', 'hbs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride("_method"))
