e2eeca372be7dca8ba6ca9f16147fde0abc632c1
king610160 <king610160@gmail.com>
2023-04-30 13:15:40 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
 const methodOverride = require("method-override")
+const flash = require('connect-flash')
 const Restaurant = require("./models/restaurant")
 const routes = require('./routes')
 
@@ -32,6 +33,15 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride("_method"))
 
 usePassport(app)
+
+app.use(flash())  // 掛載套件
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
+  next()
+})
 
 app.use((req, res, next) => {
   // 你可以在這裡 console.log(req.user) 等資訊來觀察
